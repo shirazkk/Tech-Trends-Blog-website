d0274739bb6f5aa619b01ed18b5e32f4a95d2358
Shiraz Ali <shirazkk8@gmail.com>
2025-01-06 19:30:43 +0500

    force-to-render-dynamically

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import BlogHero from "@/components/blog_components/blog_hero";
 import Blogs from "@/components/blog_components/blogs";
 
+export const dynamic = "force-dynamic";
 const Blog = () => {
   return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,11 @@ import Hero from "@/components/hero";
 import PopularPost from "@/components/popular_post";
 import RecentBlogs from "@/components/recentblog";
 
+export const dynamic = "force-dynamic";
 export default async function Home() {
   return (
     <>
-      <Hero />
+      <Hero/>
       <RecentBlogs />
       <PopularPost />
     </>
